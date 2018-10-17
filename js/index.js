b6a28d181de8ddb4acce2202d807cc507be40e92
knudsoncassy <knudsoncassy@gmail.com>
2018-10-17 15:21:30 -0500

    udate

--- js/index.js
+++
@@ -1,2 +0,0 @@
-var oeTags = '<img src="Untitled%20Projecta_media/Untitled%20Projecta.gif" width="1920" height="1080" alt=""/>';         
-document.write( oeTags );
--- thurberstudios_media/thurberstudios_player_embed.js
+++
@@ -1,2 +0,0 @@
-var oeTags = '<img src="thurberstudios_media/thurberstudios.gif" width="1500" height="843" alt=""/>';         
-document.write( oeTags );
